2dd1507b18060ee21e881d6c204fb749229c2a68
Lisa Hagin <lhagin@encompass.tv>
2017-05-10 13:42:54 -0400

    added server.js and pricing_controller setup files

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var db = require('./models');
+
+// set up server
+var app = express();
+var port = process.env.PORT || 8080;
+
+db.sequelize.sync().then(function() {
+    app.listen(port, function() {
+        console.log('listening on ' + port);
+    });
+});
+
+// set up handlebars engine
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Serve static content for the app from the 'public' directory in the
+// application directory.
+app.use(express.static(__dirname + '/public'));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+
+// get the routes
+require('./controllers/pricing_controller.js')(app);
