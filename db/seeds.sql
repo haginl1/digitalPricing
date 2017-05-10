08f25f76f7fecbc1a976b7baf9d5f889b087159f
Lisa Hagin <lhagin@encompass.tv>
2017-05-10 13:21:24 -0400

    added schema and sql

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO HD_1080 (streams, discount, year_1, year_2, year_3) 
+VALUES ('1', '0.0', '950.00', '925.00', '875.00');
+
+INSERT INTO HD_1080 (streams, discount, year_1, year_2, year_3) 
+VALUES ('2', '.02', '931', '907', '858');
+
+INSERT INTO HD_1080 (streams, discount, year_1, year_2, year_3) 
+VALUES ('26', '.05', '903', '879', '831');
+
+INSERT INTO HD_1080 (streams, discount, year_1, year_2, year_3) 
+VALUES ('51', '.10', '855', '833', '788');
+
+INSERT INTO HD_1080 (streams, discount, year_1, year_2, year_3) 
+VALUES ('101', '.15', '808', '786', '744');
+
+INSERT INTO HD_1080 (streams, discount, year_1, year_2, year_3) 
+VALUES ('151', '.20', '760', '740', '700');
