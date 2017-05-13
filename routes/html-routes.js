a20d4e4fdcdcffedd1c9613b347695200938c9c6
lhagin@comcast.net <lhagin@comcast.net>
2017-05-13 14:09:35 -0400

    added new views and edited html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,12 @@
 module.exports = function(app){
     app.get("/", function(req, res) {
-        res.render("index", {});
+        res.render("allQuotes", {});
     });
+    app.get("/result", function(req, res) {
+         res.render("result", {});
+     });
+     app.get("/quote", function(req, res) {
+         res.render("quote", {});
+     });
 }
+
