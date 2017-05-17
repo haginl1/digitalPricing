737c9408f0b66041a95876cd81f7c20abaf0986e
Jake Owens <Jowens@lawn-143-215-92-172.lawn.gatech.edu>
2017-05-16 21:16:11 -0400

    allquote data display

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,12 +1,43 @@
-module.exports = function(app){
-    app.get("/", function(req, res) {
-        res.render("allQuotes", {});
-    });
-    app.get("/result", function(req, res) {
-         res.render("result", {});
-     });
-     app.get("/quote", function(req, res) {
-         res.render("quote", {});
-     });
-}
+"use strict";
 
+var Quotes = require('../controllers/quotes_controller.js')
+
+module.exports = function(app) {
+    app.get("/", function(req, res) {
+        Quotes.getAll(function callback(allQuotes) {
+            console.log(allQuotes)
+            var result = {
+                quotes: allQuotes
+            };
+
+
+            res.render("allQuotes", result);
+        })
+    });
+
+    app.get("/result/:id", function(req, res) {
+        console.log(req.params.id);
+        Quotes.getOne(req.params.id, function callback(quote) {
+            if (quote.length === 0) {
+                res.status(404).send({
+                    result: "error",
+                    message: "Quote not found"
+                })
+            } else {
+                var resquote = {
+                    quote: quote,
+                    test: { label: "this is test" }
+
+                };
+                console.log(resquote.test.label);
+                res.render("result", { resquote });
+
+            }
+        })
+
+
+    });
+    app.get("/quote", function(req, res) {
+        res.render("quote", {});
+    });
+}
