bea361c17c6205cc0e6d872c4b1a932a587a37f7
Jake Owens <Jowens@Jakes-Air-3.attlocal.net>
2017-06-28 14:15:07 -0400

    removed login registration for existing users on quote page

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ function ensureAuthenticated(req,res,next){
 	if(req.isAuthenticated()){
 		return next();
 }else{
-	//req.flash('error_msg', 'You are not logged in');
 	res.redirect('/users/login');
 	}
 }
