240e81df0100142dcad62b19c3d84a27c068d309
Trent Marshall <Trent@lawn-128-61-48-183.lawn.gatech.edu>
2017-07-06 19:17:05 -0400

    datatable fixed

--- app/pages/QuoteHistory.js
+++ app/pages/QuoteHistory.js
@@ -5,66 +5,66 @@ import QuoteHistoryRow from "./QuoteHistoryRow.js"
 
 
 export default class QuoteHistory extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {quotes:["hello this is quotes"]}
-	}
+    constructor(props) {
+        super(props);
+        this.state = {quotes:["hello this is quotes"]}
+    }
 
-	componentDidMount(props) {
-		this.getQuotes()
-	}
+    componentDidMount(props) {
+        this.getQuotes()
+    }
 
-	getQuotes() {
-		  axios.get('/api/quotes')
-	  .then(function (response) {
-		this.setState({quotes: response.data})
-	  }.bind(this))
-	  .catch(function (error) {
-		console.log(error);
-		this.setState({quotes: []})
-	  });
-	}
+    getQuotes() {
+          axios.get('/api/quotes')
+      .then(function (response) {
+        this.setState({quotes: response.data})
+      }.bind(this))
+      .catch(function (error) {
+        console.log(error);
+        this.setState({quotes: []})
+      });
+    }
 
-	renderEachQuote() {
-		$(document).ready(function(){
-			$('#myTable').dataTable();
-		});
-		if (this.state.quotes === []) {
-			return
-		}
-		else {
-			var list = this.state.quotes.map(function(row, index){
-				return (
-					<QuoteHistoryRow key={index} quote={row} setCurrentQuote={this.props.setCurrentQuote}/>
-				)
-			}.bind(this))
-			return <tbody>{list}</tbody>	   
-		}
-	}
+    renderEachQuote() {
+        $(document).ready(function(){
+            $('#myTable').dataTable();
+        });
+        if (this.state.quotes === []) {
+            return
+        }
+        else {
+            var list = this.state.quotes.map(function(row, index){
+                return (
+                    <QuoteHistoryRow key={index} quote={row} setCurrentQuote={this.props.setCurrentQuote}/>
+                )
+            }.bind(this))
+            return <tbody>{list}</tbody>       
+        }
+    }
 
   render() {
-	return (
-	<div>
-		<div className="panel panel-primary">
-			<div className="panel-heading ">
-				<h3 className="panel-title text-warning pull-left">Quote History</h3>
-				<div className="clearfix"></div>
-			</div>
-			<div className="panel-body" id="accordion">
-				<table id="myTable"className="table table-striped table-hover">
-					<thead>
-						<tr>
-							<th><button type="button" className="btn btn-xs btn-default" id="name-btn">Date</button></th>
-							<th><button type="button" className="btn btn-xs btn-default" id="style-btn">Company</button></th>
-							<th><button type="button" className="btn btn-xs btn-default" id="rating-btn">Description</button></th>
-							<th><button type="button" className="btn btn-xs btn-default" id="abv-btn">Quotes</button></th>
-						</tr>
-					</thead>					
-							{this.renderEachQuote()}
-				</table>
-			</div>
-		</div>
-	</div>
-	);
+    return (
+    <div>
+        <div className="panel panel-primary">
+            <div className="panel-heading ">
+                <h3 className="panel-title text-warning pull-left">Quote History</h3>
+                <div className="clearfix"></div>
+            </div>
+            <div className="panel-body" id="accordion">
+                <table id="myTable"className="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th><button type="button" className="btn btn-xs btn-default" id="name-btn">Date</button></th>
+                            <th><button type="button" className="btn btn-xs btn-default" id="style-btn">Company</button></th>
+                            <th><button type="button" className="btn btn-xs btn-default" id="rating-btn">Description</button></th>
+                            <th><button type="button" className="btn btn-xs btn-default" id="abv-btn">Quotes</button></th>
+                        </tr>
+                    </thead>                    
+                            {this.renderEachQuote()}
+                </table>
+            </div>
+        </div>
+    </div>
+    );
   }
 }
