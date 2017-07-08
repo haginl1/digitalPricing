4681526bbddeacaa29fcc921379dade111bfecd7
Trent Marshall <Trent@lawn-128-61-48-183.lawn.gatech.edu>
2017-07-08 11:23:45 -0400

    calc fixes

--- app/pages/NewQuote.js
+++ app/pages/NewQuote.js
@@ -5,123 +5,122 @@ import SaveQuoteButton from './SaveQuoteButton';
 
 
 export default class Archives extends React.Component {
-<<<<<<< HEAD
-				constructor(props) {
-								super(props);
-								this.state = {
-												quoteNewEstimate:0,
-												quoteOldEstimate:0,
-												company:"",
-												description:"",
-												contractTerm: 3,
-												yearOneChannels: 1,
-												yearTwoChannels: 1,
-												yearThreeChannels: 1,
-												HLS: true,
-												HDS: false,
-												MPEG_DASH: false,
-												RTMP: false,
-												supportPlan: "Gold"
-								}
-								this.handleInputChange = this.handleInputChange.bind(this)
-								this.getEstimate = this.getEstimate.bind(this)
-				}
+        constructor(props) {
+                super(props);
+                this.state = {
+                        quoteNewEstimate:0,
+                        quoteOldEstimate:0,
+                        company:"",
+                        description:"",
+                        contractTerm: 3,
+                        yearOneChannels: 1,
+                        yearTwoChannels: 1,
+                        yearThreeChannels: 1,
+                        HLS: true,
+                        HDS: false,
+                        MPEG_DASH: false,
+                        RTMP: false,
+                        supportPlan: "Gold"
+                }
+                this.handleInputChange = this.handleInputChange.bind(this)
+                this.getEstimate = this.getEstimate.bind(this)
+        }
 
-				handleInputChange(event) {
-								const target = event.target;
-								const value = target.type === 'checkbox' ? target.checked : target.value;
-								const name = target.name;
-								this.setState({
-												[name]: value
-								}, () => {
-												this.getEstimate()
-								});
-								
-				}
+        handleInputChange(event) {
+                const target = event.target;
+                const value = target.type === 'checkbox' ? target.checked : target.value;
+                const name = target.name;
+                this.setState({
+                        [name]: value
+                }, () => {
+                        this.getEstimate()
+                });
+                
+        }
 
-				getEstimate() {
-								const APIURL =  '/api/estimate'
-																								+ "/" + this.state.contractTerm
-																								+ "/" + this.state.yearOneChannels
-																								+ "/" + this.state.yearTwoChannels
-																								+ "/" + this.state.yearThreeChannels
-																								+ "/" + this.state.HLS
-																								+ "/" + this.state.HDS
-																								+ "/" + this.state.MPEG_DASH
-																								+ "/" + this.state.RTMP
-																								+ "/" + this.state.supportPlan
-								axios.get(APIURL)
-												.then(function (response) {
-																this.setState({quoteOldEstimate: this.state.quoteNewEstimate})
-																this.setState({quoteNewEstimate: response.data.estimate})
-												}.bind(this))
-												.catch(function (error) {
-																console.log(error);
-																this.setState({quoteNewEstimate: "UNKNOWN"})
-												})
-																
-		}
+        getEstimate() {
+                const APIURL =  '/api/estimate'
+                                                + "/" + this.state.contractTerm
+                                                + "/" + this.state.yearOneChannels
+                                                + "/" + this.state.yearTwoChannels
+                                                + "/" + this.state.yearThreeChannels
+                                                + "/" + this.state.HLS
+                                                + "/" + this.state.HDS
+                                                + "/" + this.state.MPEG_DASH
+                                                + "/" + this.state.RTMP
+                                                + "/" + this.state.supportPlan
+                axios.get(APIURL)
+                        .then(function (response) {
+                                this.setState({quoteOldEstimate: this.state.quoteNewEstimate})
+                                this.setState({quoteNewEstimate: response.data.estimate})
+                        }.bind(this))
+                        .catch(function (error) {
+                                console.log(error);
+                                this.setState({quoteNewEstimate: "UNKNOWN"})
+                        })
+                                
+    }
 
-	render() {
-		return (
-			<div>
-				<div className="container-fluid ">    
-				<div className="row content">
-					<div className="col-sm-6 sidenav">
-							<div className="panel panel-primary">
-									<div className="panel-heading"><h4>Define Quote Details</h4></div>
-									<div className="panel-body">
-											<form id="main_form" className="button-size">
-									<div className="form-group">
-											<h4> 1. Enter the opportunity details.</h4>
-													<input name="company" onChange={this.handleInputChange} value={this.state.company} type="text" className="form-control" required placeholder="Company Name"></input>
-													<input name="description" onChange={this.handleInputChange} value={this.state.description} type="text" className="form-control" required placeholder="Proposal Description"></input>
-											<h4> 2. Select the protocols you need.</h4>
-											<label className="checkbox-inline">
-													<input name="HLS" checked={this.state.HLS} onChange={this.handleInputChange} type="checkbox" />HLS (Recommended)
-											</label>
-											<label className="checkbox-inline">
-													<input name="HDS" checked={this.state.HDS} onChange={this.handleInputChange} type="checkbox"/>HDS 
-											</label>
-											<label className="checkbox-inline">
-													<input name="RTMP" checked={this.state.RTMP} onChange={this.handleInputChange} type="checkbox"/>RTMP
-											</label>
-											<label className="checkbox-inline">
-													<input name="MPEG_DASH" checked={this.state.MPEG_DASH} onChange={this.handleInputChange} type="checkbox"/>MPEG-DASH
-											</label>
-											<br/>
-											
-											<br/>
-											<h4> 3. Select a contract term in years.</h4>
-											<select name="contractTerm" value={this.state.contractTerm} onChange={this.handleInputChange} className="selectpicker" id="contract_term" required title="Choose one of the following...">
-													<option id="contract_term_1">1</option>
-													<option id="contract_term_2">2</option>
-													<option defaultValue id="contract_term_3">3</option>
-											</select>
-											<h4> 4. Enter the number of streams you need each year.</h4>
-													<input name="yearOneChannels" onChange={this.handleInputChange} value={this.state.yearOneChannels} type="number" min="1" step="1" id="year_one_channels" className="form-control" required placeholder="Channels in Year 1"></input>
-													<input name="yearTwoChannels" onChange={this.handleInputChange} value={this.state.yearTwoChannels} type="number" min="1" step="1" id="year_two_channels" className="form-control"  required placeholder="Channels in Year 2"></input>
-													<input name="yearThreeChannels" onChange={this.handleInputChange} value={this.state.yearThreeChannels} type="number" min="1" step="1" id="year_three_channels" className="form-control" required placeholder="Channels in Year 3"></input>
-											<h4> 5. Select a support plan for your services.</h4>
-											<select name="supportPlan" value={this.state.supportPlan} onChange={this.handleInputChange} className="selectpicker" id="support_plan" required>
-													<option defaultValue id="support_plan_gold">Gold</option>
-													<option id="support_plan_platinum">Platinum</option>
-											</select>
-											<p></p>
-											<SaveQuoteButton setCurrentQuote={this.props.setCurrentQuote} {...this.state}/>
-									</div>
-							</form>
-									</div> 
-						</div>
-					</div>
+  render() {
+    return (
+      <div>
+        <div className="container-fluid ">    
+        <div className="row content">
+          <div className="col-sm-6 sidenav">
+              <div className="panel panel-primary">
+                  <div className="panel-heading"><h4>Define Quote Details</h4></div>
+                  <div className="panel-body">
+                      <form id="main_form" className="button-size">
+                  <div className="form-group">
+                      <h4> 1. Enter the opportunity details.</h4>
+                          <input name="company" onChange={this.handleInputChange} value={this.state.company} type="text" className="form-control" required placeholder="Company Name"></input>
+                          <input name="description" onChange={this.handleInputChange} value={this.state.description} type="text" className="form-control" required placeholder="Proposal Description"></input>
+                      <h4> 2. Select the protocols you need.</h4>
+                      <label className="checkbox-inline">
+                          <input name="HLS" checked={this.state.HLS} onChange={this.handleInputChange} type="checkbox" />HLS (Recommended)
+                      </label>
+                      <label className="checkbox-inline">
+                          <input name="HDS" checked={this.state.HDS} onChange={this.handleInputChange} type="checkbox"/>HDS 
+                      </label>
+                      <label className="checkbox-inline">
+                          <input name="RTMP" checked={this.state.RTMP} onChange={this.handleInputChange} type="checkbox"/>RTMP
+                      </label>
+                      <label className="checkbox-inline">
+                          <input name="MPEG_DASH" checked={this.state.MPEG_DASH} onChange={this.handleInputChange} type="checkbox"/>MPEG-DASH
+                      </label>
+                      <br/>
+                      
+                      <br/>
+                      <h4> 3. Select a contract term in years.</h4>
+                      <select name="contractTerm" value={this.state.contractTerm} onChange={this.handleInputChange} className="selectpicker" id="contract_term" required title="Choose one of the following...">
+                          <option id="contract_term_1">1</option>
+                          <option id="contract_term_2">2</option>
+                          <option defaultValue id="contract_term_3">3</option>
+                      </select>
+                      <h4> 4. Enter the number of streams you need each year.</h4>
+                          <input name="yearOneChannels" onChange={this.handleInputChange} value={this.state.yearOneChannels} type="number" min="1" step="1" id="year_one_channels" className="form-control" required placeholder="Channels in Year 1"></input>
+                          <input name="yearTwoChannels" onChange={this.handleInputChange} value={this.state.yearTwoChannels} type="number" min="1" step="1" id="year_two_channels" className="form-control"  required placeholder="Channels in Year 2"></input>
+                          <input name="yearThreeChannels" onChange={this.handleInputChange} value={this.state.yearThreeChannels} type="number" min="1" step="1" id="year_three_channels" className="form-control" required placeholder="Channels in Year 3"></input>
+                      <h4> 5. Select a support plan for your services.</h4>
+                      <select name="supportPlan" value={this.state.supportPlan} onChange={this.handleInputChange} className="selectpicker" id="support_plan" required>
+                          <option defaultValue id="support_plan_gold">Gold</option>
+                          <option id="support_plan_platinum">Platinum</option>
+                      </select>
+                      <p></p>
+                      <SaveQuoteButton setCurrentQuote={this.props.setCurrentQuote} {...this.state}/>
+                  </div>
+              </form>
+                  </div> 
+            </div>
+          </div>
 
-					<div className="col-sm-6 sidenav">
-									<Calc oldEstimate={this.state.quoteOldEstimate} newEstimate={this.state.quoteNewEstimate}/>
-									<img className="priceTag" src="/assets/img/ipad.png"/>
-					</div>
-				</div>
-			</div>
-			</div>
-		);
-	}
+          <div className="col-sm-6 sidenav">
+                  <Calc oldEstimate={this.state.quoteOldEstimate} newEstimate={this.state.quoteNewEstimate}/>
+                  <img className="priceTag" src="/assets/img/ipad.png"/>
+          </div>
+        </div>
+      </div>
+      </div>
+    );
+  }
 }
