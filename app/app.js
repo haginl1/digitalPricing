03bfad94e7b2e3d38bf14b02ec89694677f0a11b
Les Barchard <lbarchard@lawn-143-215-119-176.lawn.gatech.edu>
2017-07-09 10:47:50 -0400

    Just testing and playing at this point

--- app/app.js
+++ app/app.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from "./pages/Layout.js";
 import { Route, HashRouter } from "react-router-dom";
+import $ from 'jquery';
+
 
 ReactDOM.render(
   <HashRouter>
     <Route path='/' component={Layout}/>
   </HashRouter>
 , document.getElementById('root'));
+
