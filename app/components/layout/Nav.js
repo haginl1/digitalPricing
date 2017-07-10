5a65a11ae1d72486cc08d92ef1e1b1fb3d2e540d
Les Barchard <lbarchard@lad-cust-129-122-168-102.zapfibra.co.ao>
2017-07-10 19:39:22 -0400

    nav bar respects logged in/out

--- app/components/layout/Nav.js
+++ app/components/layout/Nav.js
@@ -18,17 +18,39 @@ export default class Nav extends React.Component {
     userLogout() {
       this.props.setThisUserID("")
     }
+
+    buildMenu () {
+      const { pathname } = this.props.location;
+      const historyClass = pathname === '/' ? 'active':'';
+      const quoteClass = pathname.match(/^\/quote/) ? 'active':'' ;
+      const loginClass = pathname.match(/^\/login/) ? 'active':'' ;
+
+      const registerClass = pathname.match(/^\/register/) ? 'active':'' ;
+      const logoutClass = pathname.match(/^\/logout/) ? 'active':'' ;
+      const contactClass =  pathname.match(/^\/contact/) ? 'active':'';
+      if (this.props.userID){
+            return (
+              <ul className="nav navbar-nav">
+                <li className={historyClass}><NavLink to='/'>Quote History</NavLink></li> 
+                <li className={quoteClass}><NavLink to='/quote' >New Quote</NavLink></li>
+                <li className={logoutClass}><NavLink to='/login' onClick={this.userLogout}>Logout</NavLink></li>
+                <li className={contactClass}><NavLink to='/contact'>Contact Us</NavLink></li>
+              </ul>    )}
+        else{
+          return (
+
+            <ul className="nav navbar-nav">
+                <li className={loginClass}><NavLink to='/login' >Login</NavLink></li>
+                <li className={registerClass}><NavLink to='/register'>Register</NavLink></li>
+                <li className={contactClass}><NavLink to='/contact'>Contact Us</NavLink></li>
+              </ul>    )}
+          
+        }
    
   render() {
-    const { collapsed } = this.state;
-    const { pathname } = this.props.location;
-    const navClass = collapsed ? "collapse":'';
-    const historyClass = pathname === '/' ? 'active':'';
-    const quoteClass = pathname.match(/^\/quote/) ? 'active':'' ;
-    const loginClass = pathname.match(/^\/login/) ? 'active':'' ;
-    const registerClass = pathname.match(/^\/register/) ? 'active':'' ;
-    const logoutClass = pathname.match(/^\/logout/) ? 'active':'' ;
-    const contactClass =  pathname.match(/^\/contact/) ? 'active':'';
+       const navClass = collapsed ? "collapse":'';
+      const { collapsed } = this.state;
+
     return (
       <nav className="navbar navbar-inverse navbar-fixed-top">
         <div className="container-fluid">
@@ -40,20 +62,10 @@ export default class Nav extends React.Component {
               data-target="#navbarContent" 
               aria-expanded="false">
                 <span className="sr-only">Toggle navigation</span>
-            
             </button>
           </div>
           <div className={"navbar-collapse " + navClass} id="navbarContent">
-            <ul className="nav navbar-nav">
-             
-              <li className={registerClass}><NavLink to='/register'>Register</NavLink></li>
-
-              <li className={historyClass}><NavLink to='/'>Quote History</NavLink></li> 
-              <li className={quoteClass}><NavLink to='/quote' >New Quote</NavLink></li>
-              <li className={loginClass}><NavLink to='/login' >Login</NavLink></li>
-              <li className={logoutClass}><NavLink to='/login' onClick={this.userLogout}>Logout</NavLink></li>
-              <li className={contactClass}><NavLink to='/contact'>Contact Us</NavLink></li>
-            </ul>
+            {this.buildMenu()}
           </div>
         </div>
       </nav>
