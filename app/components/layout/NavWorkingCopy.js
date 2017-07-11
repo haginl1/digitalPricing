73d5aeaebc4581143435f28204ec7f2234176e99
Jake Owens <Jowens@Jakes-MacBook-Air-3.local>
2017-07-10 20:43:23 -0400

    fix NewQuote estimate for year 2 & 3

---
+++ app/components/layout/NavWorkingCopy.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default class Nav extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      collapsed: true,
+      valMenu: ""
+    }
+     this.userLogout=this.userLogout.bind(this)
+  }
+
+ //using state instead of jQuery to toggle top nav for mobile browsers
+  toggleClass(){
+    const collapsed = !this.state.collapsed;
+    this.setState({collapsed});
+  }
+
+  userLogout() {
+    this.props.setThisUserID("")
+    console.log(this.props.setThisUserID);
+  }
+   
+
+  validatedUserMenu() {
+
+    console.log("in validateduserMenu");
+
+    const { collapsed } = this.state;
+    const { pathname } = this.props.location;
+    const navClass = collapsed ? "collapse":'';
+    const historyClass = pathname === '/' ? 'active':'';
+    const quoteClass = pathname.match(/^\/quote/) ? 'active':'' ;
+    const loginClass = pathname.match(/^\/login/) ? 'active':'' ;
+    const registerClass = pathname.match(/^\/register/) ? 'active':'' ;
+    const logoutClass = pathname.match(/^\/logout/) ? 'active':'' ;
+    const contactClass =  pathname.match(/^\/contact/) ? 'active':'';
+    
+           return (
+            <ul className="nav navbar-nav">
+              <li className={registerClass}><NavLink to='/register'>Register</NavLink></li>
+              <li className={historyClass}><NavLink to='/'>Quote History</NavLink></li> 
+              <li className={quoteClass}><NavLink to='/quote' >New Quote</NavLink></li>
+              <li className={loginClass}><NavLink to='/login' >Login</NavLink></li>
+              <li className={logoutClass}><NavLink to='/login' onClick={this.userLogout}>Logout</NavLink></li>
+              <li className={contactClass}><NavLink to='/contact'>Contact Us</NavLink></li>
+            </ul> );
+   
+  }
+
+  render() {
+  
+    return (
+      <nav className="navbar navbar-inverse navbar-fixed-top">
+        <div className="container-fluid">
+          <div className="navbar-header">
+          <a className="navbar-brand" href="/"><img src ="./assets/img/logo-50.png" alt="logo"/></a>
+            <button type="button" className="navbar-toggle collapsed" 
+              onClick={this.toggleClass.bind(this)} 
+              data-toggle="collapse" 
+              data-target="#navbarContent" 
+              aria-expanded="false">
+                <span className="sr-only">Toggle navigation</span>
+            </button>
+          </div>
+          <div className={"navbar-collapse " + navClass} id="navbarContent">
+      
+          </div>
+          <div>
+              {this.validatedUserMenu()}
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
---
+++ app/pages/LayoutWorkingCopy.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Route, Switch } from "react-router-dom";
+import QuoteHistory from "./QuoteHistory.js";
+import NewQuote from "./NewQuote.js";
+import NotFound from "./404.js";
+import Contact from "./Contact.js";
+import Nav from "../components/Layout/Nav.js";
+import Footer from "../components/Layout/Footer.js";
+import QuoteDetails from "./QuoteDetails.js";
+import Register from "./Register.js";
+import Login from "./Login.js";
+import Logout from "./Logout.js";
+
+
+export default class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userID:"123",
+      currentQuote:""
+
+    }
+    this.setCurrentQuote = this.setCurrentQuote.bind(this)
+    this.setCurrentUserID = this.setCurrentUserID.bind(this)
+  }
+
+  setCurrentQuote(row) {
+    this.setState({currentQuote: row})
+  }
+
+  setCurrentUserID(userid) {
+    console.log('in setcurrentuser');
+    console.log(userid);
+    this.setState({userID: userid})
+    console.log('set state');
+    console.log(this.state.userID);
+  }
+
+  render() {
+    const { location } = this.props;
+
+    return (
+      <div>
+        <Nav location={location} />
+        <div className='container'>
+          <div>
+            <Switch> 
+
+            { /* <Route path='/quote' component={NewQuote} />*/}
+            <Route path='/login' component={Login}/> 
+
+              <Route path='/logout' component={Logout} />
+          {/*  <Switch> */}
+              <Route exact path='/quote' render={routeProps => <NewQuote {...routeProps} setCurrentQuote={this.setCurrentQuote} embedded={false} userID={this.state.userID} currentQuote={this.state.currentQuote}/>} />
+              <Route path='/contact' component={Contact} />
+              <Route exact path='/details' render={routeProps => <QuoteDetails {...routeProps} currentQuote={this.state.currentQuote}/>} />
+          <Route exact path='/' render={routeProps => <QuoteHistory {...routeProps} setCurrentQuote={this.setCurrentQuote} currentQuote={this.state.currentQuote}/>} /> 
+             <Route exact path='/register' render={routeProps => <Register {...routeProps} setThisUserID={this.setCurrentUserID}/>} /> 
+           {/* <Route exact path='/' component={Login} />*/}
+              <Route exact path='*' component={NotFound} />
+            </Switch>
+          </div>
+          <div>
+            <Footer />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
