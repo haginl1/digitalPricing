fb72ab34421b5a6ea0bab092fdacea651b245cf7
Trent Marshall <Trent@lawn-128-61-48-183.lawn.gatech.edu>
2017-07-15 07:43:02 -0400

    embedded fix

--- app/pages/NewQuote.js
+++ app/pages/NewQuote.js
@@ -21,9 +21,10 @@ export default class Archives extends React.Component {
                 RTMP: false,
                 supportPlan: "Gold"
     }
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.getEstimate = this.getEstimate.bind(this)
-    this.showSaveQuoteButton = this.showSaveQuoteButton.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.getEstimate = this.getEstimate.bind(this);
+    this.showSaveQuoteButton = this.showSaveQuoteButton.bind(this);
+    this.showDescription = this.showDescription.bind(this);
 }
 
     handleInputChange(event) {
@@ -36,7 +37,18 @@ export default class Archives extends React.Component {
                         this.getEstimate()
         });
     }
+    showDescription() {
+        if (this.props.embedded === false) {
+            return (
+                    <div>
+                    <h4>Enter the opportunity details.</h4>
+                    <input name="company"  value={this.state.company} type="text" className="form-control" required placeholder="Company Name"></input>
+                    <input name="description"  value={this.state.description} type="text" className="form-control" required placeholder="Proposal Description"></input>  
+                    </div>
 
+            )
+        }
+    }
     showSaveQuoteButton() {
         if (this.props.embedded === false) {
             return (<SaveQuoteButton userID={this.props.userID} setCurrentQuote={this.props.setCurrentQuote} {...this.state}/>)
@@ -76,10 +88,8 @@ export default class Archives extends React.Component {
                                 <div className="panel-body">
                                     <form id="main_form" className="button-size" onChange={this.handleInputChange}>
                                         <div className="form-group">
-                                            <h4> 1. Enter the opportunity details.</h4>
-                                            <input name="company"  value={this.state.company} type="text" className="form-control" required placeholder="Company Name"></input>
-                                            <input name="description"  value={this.state.description} type="text" className="form-control" required placeholder="Proposal Description"></input>
-                                            <h4> 2. Select the protocols you need.</h4>
+                                            {this.showDescription()}
+                                            <h4>Select the protocols you need.</h4>
                                             <label className="checkbox-inline">
                                                 <input name="HLS" checked={this.state.HLS}  type="checkbox" />HLS (Recommended)
                                             </label>
@@ -95,17 +105,17 @@ export default class Archives extends React.Component {
                                             <br/>
                                             
                                             <br/>
-                                            <h4> 3. Select a contract term in years.</h4>
+                                            <h4>Select a contract term in years.</h4>
                                             <select name="contractTerm" value={this.state.contractTerm}  className="selectpicker" id="contract_term" required title="Choose one of the following...">
                                                 <option id="contract_term_1">1</option>
                                                 <option id="contract_term_2">2</option>
                                                 <option defaultValue id="contract_term_3">3</option>
                                             </select>
-                                            <h4> 4. Enter the number of streams you need each year.</h4>
+                                            <h4>Enter the number of streams you need each year.</h4>
                                             <input name="yearOneChannels"  value={this.state.yearOneChannels} type="number" min="1" step="1" id="year_one_channels" className="form-control" required placeholder="Channels in Year 1"></input>
                                             <input name="yearTwoChannels"  value={this.state.yearTwoChannels} type="number" min="1" step="1" id="year_two_channels" className="form-control"  required placeholder="Channels in Year 2"></input>
                                             <input name="yearThreeChannels"  value={this.state.yearThreeChannels} type="number" min="1" step="1" id="year_three_channels" className="form-control" required placeholder="Channels in Year 3"></input>
-                                            <h4> 5. Select a support plan for your services.</h4>
+                                            <h4>Select a support plan for your services.</h4>
                                             <select name="supportPlan" value={this.state.supportPlan}  className="selectpicker" id="support_plan" required>
                                                 <option defaultValue id="support_plan_gold">Gold</option>
                                                 <option id="support_plan_platinum">Platinum</option>
